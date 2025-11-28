5a5668f9592bdad3ea67904bef5a8ea99a29abba
Eduardo Almeida <almeedusa@gmail.com>
2025-11-28 18:32:37 -0300

    Testes com ambas as Classes

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from conta import Conta
+from conta_poupanca import ContaPoupanca
 from conta_corrente import ContaCorrente
 
 if __name__ == "__main__":
-    conta1 = ContaCorrente(1,1)
-    conta1.depositar(10.00)
+    conta1 = ContaPoupanca(1,1)
+    conta1.depositar(100.00)
     
     conta1.extrato()
     
     conta1.sacar(10.00)
     conta1.extrato()
     
-    conta1.sacar(100.00)
+    conta1.sacar(50.00)
     conta1.extrato()
     
