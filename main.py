e515fffc9998cf0afca678471df93c48e0769970
Eduardo Almeida <almeedusa@gmail.com>
2025-11-28 17:47:39 -0300

    Teste classe ContaCorrente

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from conta import Conta
+from conta_corrente import ContaCorrente
 
 if __name__ == "__main__":
-    conta1 = Conta(1,1)
-    conta1.depositar(12)
+    conta1 = ContaCorrente(1,1)
+    conta1.depositar(10.00)
     
     conta1.extrato()
     
-    conta1.sacar(10)
+    conta1.sacar(10.00)
     conta1.extrato()
+    
+    conta1.sacar(100.00)
+    conta1.extrato()
+    
