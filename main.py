ba860cba8f3ffb58a89e3bffaf6cf518266ac079
Eduardo Almeida <almeedusa@gmail.com>
2025-12-15 18:10:32 -0300

    Testes de objetos 'clientes'

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from conta_poupanca import ContaPoupanca
 from conta_corrente import ContaCorrente
+from cliente import Cliente
 
 if __name__ == "__main__":
-    conta1 = ContaPoupanca(1,1)
-    conta1.depositar(100.00)
-    
-    conta1.extrato()
-    
-    conta1.sacar(10.00)
-    conta1.extrato()
-    
-    conta1.sacar(50.00)
-    conta1.extrato()
-    
+    cliente = Cliente('Eduardo', 23)
+    cliente.conta = ContaCorrente(111, 123)
+    cliente.conta.depositar(120)
+    cliente.conta.extrato()
+    print(cliente.conta)
+    print(cliente)
+
+    cliente2 = Cliente('Larissa', 28)
+    cliente2.conta = ContaPoupanca(111, 124)
+    cliente2.conta.depositar(120)
+    cliente2.conta.extrato()
+    print(cliente2.conta)
+    print(cliente2)
+
