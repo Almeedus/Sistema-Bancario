7226fcff7b2b5362d6f16c11153a528f6134db0a
Eduardo Almeida <almeedusa@gmail.com>
2025-12-17 19:06:25 -0300

    Teste do sistema final

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from conta_poupanca import ContaPoupanca
-from conta_corrente import ContaCorrente
-from cliente import Cliente
+from banco import Banco, ContaPoupanca, ContaCorrente, Cliente
 
 if __name__ == "__main__":
-    cliente = Cliente('Eduardo', 23)
-    cliente.conta = ContaCorrente(111, 123)
-    cliente.conta.depositar(120)
-    cliente.conta.extrato()
-    print(cliente.conta)
-    print(cliente)
+    santander = Banco('Santander', 111)
+    
+    pessoa1 = Cliente('Eduardo', 23)
+    pessoa1.conta = ContaCorrente(111,123)
 
-    cliente2 = Cliente('Larissa', 28)
-    cliente2.conta = ContaPoupanca(111, 124)
-    cliente2.conta.depositar(120)
-    cliente2.conta.extrato()
-    print(cliente2.conta)
-    print(cliente2)
+    pessoa2 = Cliente('Larissa', 28)
+    pessoa2.conta = ContaPoupanca(111,11001)
+    
+    santander.clientes.extend([pessoa1, pessoa2])
+    santander.contas.extend([pessoa1.conta, pessoa2.conta])
 
+    if santander.autenticacao(pessoa1, pessoa2.conta):
+        pessoa1.conta.extrato()
+        
+        pessoa1.conta.sacar(100)
+        
+        pessoa1.conta.extrato()
+    else:
+        print(f'Autenticação Inválida')
